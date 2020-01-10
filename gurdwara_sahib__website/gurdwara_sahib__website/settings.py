6f4f39008d07b5437de2519d3087f7c7fb5a5a1f
Rubinder Singh <46024206+rubindersinghsandhu@users.noreply.github.com>
2020-01-10 19:46:06 +0530

    Update settings.py

--- gurdwara_sahib__website/gurdwara_sahib__website/settings.py
+++ gurdwara_sahib__website/gurdwara_sahib__website/settings.py
@@ -120,17 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-
-if DEBUG:
-    STATIC_URL = '/static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static/rfiles')
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-    TEMPLATE_DIRS = (
-        os.path.join(BASE_DIR,  'templates'),
-        'gurdwara_sahib__website/gsmcdhigana/templates/html',
-    )
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
-    ]
-    django_heroku.settings(locals())
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/rfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+    'gurdwara_sahib__website/gsmcdhigana/templates/html',
+)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+django_heroku.settings(locals())
