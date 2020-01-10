ed673cb315d79fdd6888d64598ba7d2bdf0fcfb0
Rubinder Singh <46024206+rubindersinghsandhu@users.noreply.github.com>
2020-01-10 18:37:43 +0530

    Update settings.py

--- gurdwara_sahib__website/gurdwara_sahib__website/settings.py
+++ gurdwara_sahib__website/gurdwara_sahib__website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG_VALUE")=="True")
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'gsmc.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
