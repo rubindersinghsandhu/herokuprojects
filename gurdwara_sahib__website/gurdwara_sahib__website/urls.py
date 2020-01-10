84d4fc29d9d1b0becbb5b3636cb694399d3934db
Rubinder Singh <46024206+rubindersinghsandhu@users.noreply.github.com>
2020-01-10 18:26:37 +0530

    Update urls.py

--- gurdwara_sahib__website/gurdwara_sahib__website/urls.py
+++ gurdwara_sahib__website/gurdwara_sahib__website/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('gsmcdhigana.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
